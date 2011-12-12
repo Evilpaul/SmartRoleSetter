10c36f28499f751adfac2f91e2c8a695c3ae40db
Evilpaul <yj589794@googlemail.com>
2011-12-12 05:22:41 +0000

    remove saved options, command line options and onupdate

--- SmartRoleSetter.lua
+++ SmartRoleSetter.lua
@@ -1,12 +1,8 @@
-SLASH_SMARTROLESETTER1 = '/srs';
-SLASH_SMARTROLESETTER2 = '/srole';
-
 local sRoleDmg	= 'DAMAGER';
 local sRoleTank = 'TANK';
 local sRoleHeal = 'HEALER';
 local sRoleNone = 'NONE';
 
-local _O	= nil;
 local frame = nil;
 local role = 'NONE';
 local isRoleSet = false;
@@ -79,14 +75,13 @@ end
 
 function SmartRoleSetter_OnEvent(self, event, ...)
 	if (not isInit) then return; end
-	if (not _O.isOn) then return; end
 
 	if (event == 'PLAYER_TALENT_UPDATE') then
 		CheckRole(true);
 		SetRole(role, false);
 	end
 
-	if (event == 'ROLE_POLL_BEGIN' and _O.isAuto) then
+	if (event == 'ROLE_POLL_BEGIN') then
 		role = sRoleNone;
 		CheckRole(true);
 		isRoleSet = false;
@@ -100,60 +95,8 @@ function SmartRoleSetter_OnEvent(self, event, ...)
 end
 
 
-function SmartRoleSetter_OnUpdate(self, elapsed)
-	tTime = tTime + elapsed;
-	if (tTime > 1.0) then
-		tTime = 0.0;
-		if (isInit) then
-			if (_O.isOn and doCheck) then
-				doCheck = false;
-				CheckRole(false);
-				SetRole(role, false);
-			end
-		else
-			local sName = GetTalentInfo(1, 1);
-			if (sName) then
-				-- Settings --------------------
-				if (SMARTROLESETTER_Options == nil) then SMARTROLESETTER_Options = { }; end
-				_O = SMARTROLESETTER_Options;
-				if (_O.isOn == nil) then _O.isOn = true; end
-				if (_O.isAuto == nil) then _O.isAuto = true; end
-				print('SmartRoleSetter loaded');
-				--------------------------------
-				isInit = true;
-				doCheck = true;
-			end
-		end
-	end
-end
-
-
-function SlashCmdList.SMARTROLESETTER(...)
-	local msg = ...;
-	if (msg == '') then
-		_O.isOn = not _O.isOn;
-		print('SmartRoleSetter active = '..tostring(_O.isOn));
-		if (_O.isOn) then
-			CheckRole(true);
-			SetRole(role, false);
-		end
-	elseif (msg == 'auto') then
-		_O.isAuto = not _O.isAuto;
-		print('SmartRoleSetter auto accept role poll = '..tostring(_O.isAuto));
-	elseif (msg == 't1') then
-		RolePollPopup_Show(RolePollPopup);
-	elseif (msg == 't2') then
-		StaticPopupSpecial_Hide(RolePollPopup);
-	else
-		print('SmartRoleSetter:');
-		print('Active = '..tostring(_O.isOn));
-		print('Auto accept role poll = '..tostring(_O.isAuto));
-	end
-end
-
 frame = CreateFrame('Frame', 'SmartRoleSetterFrame', UIParent);
 frame:SetScript('OnEvent', SmartRoleSetter_OnEvent);
-frame:SetScript('OnUpdate', SmartRoleSetter_OnUpdate);
 frame:RegisterEvent('PLAYER_TALENT_UPDATE');
 frame:RegisterEvent('RAID_ROSTER_UPDATE');
 frame:RegisterEvent('ROLE_POLL_BEGIN');
